3118c55caafcea9c1086cd72564849c3a645c888
camillamanzo <camillasouthafrica@gmail.com>
2021-09-21 12:39:47 +0200

    initialized milestone 3

--- js/script.js
+++ js/script.js
@@ -90,20 +90,59 @@ let app = new Vue(
 
         ],
         indexContacts: 0,
+        newMessage: "",
+    
     },
     methods: {
-        //Function that changes the object element "avatar" in its corrisponding img by its
+        //Function that changes the object element "avatar" in its corrisponding img.
         addImages: function (imgAvatar){
             return "./img 3/avatar" + this.contacts[imgAvatar].avatar + ".jpg";
         },
+
         // Function that changes messages based on the clicked person.
         selectedContact: function (index){
             this.indexContacts = index;
         },
 
+        // Function that returns the last message's date.
         lastSeen: function (){
             let lastMessage = this.contacts[this.indexContacts].messages[this.contacts[this.indexContacts].messages.length - 1].date;
             return lastMessage;
+        },
+
+        // Function that gets today's date.
+        newMessageDate: function(){
+            today = new Date();
+            let date = today.getDate() + '/' + (today.getMonth()+1) + '/' + today.getFullYear();
+            let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            let date_time = date + ' ' + time;
+            return date_time;
+        },
+
+        //Function that pushes new object in current object's array.
+        newArrayElement: function(){
+            let newSentMessage = {
+                date: this.newMessageDate(),
+                text: this.newMessage,
+                status: "sent",
+            };
+
+            let newReceivedMessage = {
+                date: this.newMessageDate(),
+                text: "Okay",
+                status: "received",
+            };
+
+            if(this.newMessage.trim(" ").length > 0){
+                this.contacts[this.indexContacts].messages.push(newSentMessage);
+                this.newMessage = "";
+
+                setTimeout(function(){
+                    this.contacts[this.indexContacts].messages.push(newReceivedMessage)
+                },1000)
+            }
         }
+
+        
     }
 })
